387892e9274f179bfa62672d36615e3df276d9f7
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-07 15:33:45 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,44 @@
+export const item = {
+    "id": "5c2eb280-02f2-4058-ac25-ee02f8831a7a",
+    "curriculum_list": [
+        {
+            "id": "3bcf574d-e87d-4e75-a904-7aceb25994eb",
+            "curriculum_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png",
+            "curriuculum_title": "Kitchen put grow my move. Rule development fire fine likely factor again. Data year human population leader her.",
+            "about_curriculum": "View number world owner how respond. Choice garden right report movement strategy. Leader art thousand cause performance next assume city."
+        },
+        {
+            "id": "6bf431c9-af5e-4f7b-ad99-674b9d19a813",
+            "curriculum_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/24.png",
+            "curriuculum_title": "Explain cause specific every probably view stock. Father exactly cup standard gas special. Summer remember material town.",
+            "about_curriculum": "Cover else never individual carry data. Cover lay dark poor player."
+        },
+        {
+            "id": "d285caa1-3c6a-453f-8295-12e5882be209",
+            "curriculum_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/359.png",
+            "curriuculum_title": "Image single success under worker. Tax their successful north leader management early.",
+            "about_curriculum": "Right tree fire early drug theory. Response parent section type kind. Yourself develop example about fact."
+        },
+        {
+            "id": "a92c0690-d48f-4e83-a7da-2c4ab1789398",
+            "curriculum_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png",
+            "curriuculum_title": "Miss rock push business. Rest open stock. Radio next listen arm wall unit ahead war. Cup tree public read what identify pay team.",
+            "about_curriculum": "Rise exist nearly game serve. Vote money throw soon."
+        },
+        {
+            "id": "b8bd616d-1a2a-42bb-bfc4-43d27e5bf151",
+            "curriculum_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/326.png",
+            "curriuculum_title": "Hold fill head make. Wall course building include.",
+            "about_curriculum": "There class show create week outside. Push structure international national plan staff happy."
+        }
+    ],
+    "month": 8586,
+    "month_text": "laugh",
+    "price": 9553,
+    "year": 585,
+    "year_text": "threat",
+    "cancelled": 6379,
+    "charge": 5922,
+    "price_reduced": 1071,
+    "price_summary": "Work issue chair minute health until."
+}
